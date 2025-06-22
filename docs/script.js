59e21478e0c24ed97e7efb7d8f1de510d018e0a7
22903 <0222shan@gmail.com>
2025-06-22 07:27:19 +0000

    add docs folder

--- docs/script.js
+++ docs/script.js
@@ -3,3 +3,8 @@ function playVoice() {
   audio.play();
   alert("正在播放語音引導⋯⋯你準備好了嗎？🥺");
 }
+function playVoice(name){
+  const audio = document . getElementById('manualAudio');
+  audio.src = `voice/manul/${name}.mp3`;
+  audio.play();
+}
