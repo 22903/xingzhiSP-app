324840aace8b69d8344a25de053ff220a6af245a
22903 <0222shan@gmail.com>
2025-06-22 18:46:07 +0800

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -8,3 +8,17 @@ function playVoice(name){
   audio.src = `voice/manul/${name}.mp3`;
   audio.play();
 }
+const voiceMap = {
+  intro: "intro.mp3",
+  guide1_wake: "guide1_wake.mp3",
+  guide2_touch: "guide2_touch.mp3",
+  guide3_hold: "guide3_hold.mp3",
+  guide4_whimper: "guide4_whimper.mp3",
+  guide5_praise: "guide5_praise.mp3"
+};
+
+function playVoice(key) {
+  const audio = document.getElementById("manualAudio");
+  audio.src = "audio/" + voiceMap[key];
+  audio.play();
+}
